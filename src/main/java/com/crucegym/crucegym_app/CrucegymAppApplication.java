7a973dcb4f0a23cdcbe58265b4900ac1e50a98ee
David Delgado <daviddelgadocabrera@alumno.ieselrincon.es>
2025-01-22 15:46:18 +0000

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/crucegym/crucegym_app/CrucegymAppApplication.java
@@ -0,0 +1,13 @@
+package com.crucegym.crucegym_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrucegymAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrucegymAppApplication.class, args);
+	}
+
+}
